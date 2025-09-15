0ef8b1562a7690900cbd1be8a23b89131d1f783f
ANTMOD46 <juntarisil55@gmail.com>
2025-09-15 10:00:49 +0700

    A

--- advanced_app/lib/di/injector.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get_it/get_it.dart';
-import '../core/services/counter_service.dart';
-
-final getIt = GetIt.instance;
-
-void setupLocator() {
-  // ลงทะเบียนเป็น singleton ครั้งเดียว ใช้ทั้งแอป
-  getIt.registerSingleton<CounterService>(CounterService());
-}
